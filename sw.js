63eff6182c94a5446f8c8b8f62a70e4b156ff3c1
aa0978929727-sudo <aa0978929727@gmail.com>
2026-08-26 09:17:06 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='my-money-v2';
+const CACHE='my-money-v3';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
